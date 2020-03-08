e8f911e9d9a47c7e121991aa467f9241213452b9
johnofhyrule <johnroman7@mac.com>
2020-03-07 20:05:46 -0800

    updates user controller js

---
+++ controllers/userController.js
@@ -0,0 +1,62 @@
+const db = require('../models');
+
+const test = (request, response) => {
+    response.json({ message: 'Test' });
+};
+
+const index = (request, response) => {
+    db.User.find({}, (error, allUsers) => {
+        if (error) return response.error(500, 'Something went wrong. Try again.');
+        response.success(200, allUsers);
+    });
+};
+
+const create = (request, response) => {
+    db.User.create(request.body, (error, createdUser) => {
+        if (error) {
+            // ALWAYS RETURN TO EXIT
+            return response
+                .status(500)
+                .json({ message: 'Something went wrong. Try again.', error: error });
+        }
+        response.success(201, createdUser);
+    });
+};
+
+const update = (request, response) => {
+    db.User.findByIdAndUpdate(
+        request.params.id,
+        request.body,
+        { new: true },
+        (error, updatedUser) => {
+            if (error) {
+                // ALWAYS RETURN TO EXIT
+                return response
+                    .status(500)
+                    .json({ message: 'Something went wrong. Try again.', error: error});
+            }
+            response.success(200, updatedUser);
+        }
+    );
+};
+
+const destroy = (request, response) => {
+    db.User.findByIdAndDelete(request.params.id, (error, deletedUser) => {
+        if (error) {
+            // ALWAYS RETURN TO EXIT
+            return response
+                .status(500)
+                .json({ message: 'Something went wrong. Try again', error: error});
+        }
+        response.success(200, deletedUser);
+    });
+};
+
+module.exports = {
+    test,
+    index,
+    create,
+    show,
+    update,
+    destroy
+};
