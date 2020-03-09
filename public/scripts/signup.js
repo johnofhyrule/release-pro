8bacda92679927341af69f1f757eda9ff88625fb
johnofhyrule <johnroman7@mac.com>
2020-03-09 12:20:14 -0700

    adds signup logic

---
+++ public/scripts/signup.js
@@ -0,0 +1,61 @@
+const form = document.getElementById('signupForm');
+
+// --------- SUBMIT EVENT LISTENER
+form.addEventListener('submit', handleSignupSubmit);
+
+// --------- HANDLE SUBMIT
+function handleSignupSubmit(event) {
+    let formIsValid = true;
+    const userData = {};
+    event.preventDefault();
+
+    // Clear Alert Messages
+    document.querySelectorAll('.alert').forEach((alert) => alert.remove());
+
+    const formInputs = [...form.elements];
+    formInputs.forEach((input) => {
+        input.classList.remove('input-error');
+        if (input.type !== 'submit' && input.value === '') {
+            formIsValid = false;
+            // Add red border to invalid input
+            input.classList.add('input-error');
+            // Add error message below input
+            input.insertAdjacentHTML('afterend', `
+                <div class="alert ${input.id}-mesage">
+                Please enter your ${input.placeholder}
+                </div>
+            `)
+        } else if (input.type === 'password' && input.value.length < 4) {
+            formIsValid = false;
+            // Add reborder to invalid input
+            input.classList.add('input-error');
+            // Add error message below input
+            input.insertAdjacentHTML('afterend', `
+                <div class="alert ${input.id}-messge">
+                Password must be at least 4 characters.
+                </div>
+            `)
+        };
+
+        if (formIsValid) {
+            userData[input.name] = input.value;
+        }
+    });
+
+    if (formIsValid) {
+        // SUBMIT DATA TO SERVER
+        fetch('/api/v1/signup', {
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+        })
+            .then((dataStream) => dataStream.json())
+            .then((dataObj) => {
+                console.log(dataObj);
+                window.location = '/login';
+            })
+            .catch((error) => console.log(error));
+    }
+}
